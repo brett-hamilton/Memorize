1e07a21f04926467fa3cb6765ecf2bfc1302ff02
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-01 16:16:02 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Brett Hamilton on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
