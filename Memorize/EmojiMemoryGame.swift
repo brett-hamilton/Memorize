08b11503e7748bb6570d6f3da9d53ad1de04a4b5
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-03 16:06:02 -0400

    Created ViewModel file

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Brett Hamilton on 5/3/25.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+}
---
+++ Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Memorize
+//
+//  Created by Brett Hamilton on 5/3/25.
+//
+
