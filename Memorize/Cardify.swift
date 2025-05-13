6eaa61fd0743e03c798e2a52ecd7dd977c2057e0
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-13 10:17:43 -0400

    Cardify

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,32 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Brett Hamilton on 5/13/25.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    let isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                content
+                
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+    }
+    
+    private struct Constants {
+        static let cornerRadius: CGFloat = 12
+        static let lineWidth: CGFloat = 2
+    }
+}
