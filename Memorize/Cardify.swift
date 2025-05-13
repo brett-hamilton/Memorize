a7c1e6ff4fe5abbca746168e2d95953ecebe6fab
Brett Hamilton <bretthamilton024@gmail.com>
2025-05-13 10:30:13 -0400

    Simplify Cardify

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -14,19 +14,23 @@ struct Cardify: ViewModifier {
         ZStack {
             let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
             
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                content
-                
-            }
-            .opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1)
+            base.strokeBorder(lineWidth: Constants.lineWidth)
+                .background(base.fill(.white))
+                .overlay(content)
+                .opacity(isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(isFaceUp ? 0 : 1)
         }
     }
     
     private struct Constants {
         static let cornerRadius: CGFloat = 12
         static let lineWidth: CGFloat = 2
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        modifier(Cardify(isFaceUp: isFaceUp))
     }
 }
